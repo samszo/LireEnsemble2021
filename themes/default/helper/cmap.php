5e1518a20d54d9aa8a24f2746437b3cc00f71e3d
thihangnga nguyen <hangnga352@gmail.com>
2021-08-11 16:01:15 +0200

    fixer_bugs_non_carte

--- themes/default/helper/cmap.php
+++ themes/default/helper/cmap.php
@@ -28,6 +28,10 @@ class cmap extends AbstractHelper
         $this->api = $this->getView()->api();
         $this->proLinks = array();
 
+        $result_carte = array();
+        $lst_item_set = array();
+        $params = array();
+
         // creer new carte
         if (isset($_POST['name_carte'])) {
             $this->add_carte();
@@ -38,96 +42,101 @@ class cmap extends AbstractHelper
             'item_set_id'=>$this->default_collection, // collection cmap
         ];
         $items_set = $this->api->search('items',$query_items_set,['limit'=>0])->getContent();
-        foreach ($items_set as $i_s) {
-            // couper nom si long
-            $str_nom = $i_s->title();
-            if (strlen($str_nom) > 30) {
-                $str_nom = substr($str_nom, 0, 30) . '...';
+        if (count($items_set) > 0) {
+            foreach ($items_set as $i_s) {
+                // couper nom si long
+                $str_nom = $i_s->title();
+                if (strlen($str_nom) > 30) {
+                    $str_nom = substr($str_nom, 0, 30) . '...';
+                }
+                $lst_item_set[$i_s->id()] = $str_nom;
+                $this->default_carte = $i_s->id();
             }
-            $lst_item_set[$i_s->id()] = $str_nom;
-            $this->default_carte = $i_s->id();
+            asort($lst_item_set);
+
+            $params = isset($_POST['cartes']) ? $_POST['cartes'] : $this->default_carte; // default carte fin
+
+            // prendre donnee de carte: "Contenu"
+            $query = [
+                'id' => $params,
+            ];
+            $items = $this->api->search('items', $query, ['limit' => 0])->getContent();
+
+            foreach ($items as $i) {
+                $result_carte[] = $this->getCarteInfo($i);
+            }
         }
-        asort($lst_item_set);
-
-        $params = isset($_POST['cartes']) ? $_POST['cartes'] : $this->default_carte; // default carte fin
-
-        // prendre donnee de carte: "Contenu"
-        $query = [
-            'id'=>$params,
-        ];
-        $items = $this->api->search('items',$query,['limit'=>0])->getContent();
-
-        foreach ($items as $i) {
-            $result_carte[] = $this->getCarteInfo($i);
-        }
-
-        $arr_entites = array();
-        $arr_id_rs = array();
-        foreach ($result_carte[0]['nodes'] as $entites) {
-            $arr_entites[$entites['idResource']]['x'] = $entites['x'];
-            $arr_entites[$entites['idResource']]['y'] = $entites['y'];
-            $arr_entites[$entites['idResource']]['id_entite'] = $entites['id'];
-            $arr_id_rs[] = $entites['idResource'];
-        }
-
-        $query_rs = [
-            'id' => $arr_id_rs,
-        ];
-        $resource_templates = $this->api->search('resource_templates',$query_rs,['limit'=>0])->getContent();
 
         $tables = array();
         $links = array();
-        foreach ($resource_templates as $key=>$rt) {
-            //récupère les propriétés
-            $pros = $rt->resourceTemplateProperties();
-            $cols = array();
-            foreach ($pros as $pro) {
-                $p = $pro->property();
-                $str_title = strlen($p->label()) > 17 ? substr($p->label(), 0, 17) . "..." : $p->label();
-                $cols[] = [
-                    'itemName'=>ucfirst($str_title)
-                    ,'id'=>$p->id()
-                    ,'links'=>$this->getPropertyLinks($p)                
-                ];
-            }
-            $str_pro = strlen($rt->label()) > 17 ? substr($rt->label(), 0, 17) . "..." : $rt->label();
-            if (!isset($arr_entites[$rt->id()]['x']) || !isset($arr_entites[$rt->id()]['y'])) {
-                $tables[] = [
-                    'tableName'=>ucfirst($str_pro),
-                    'id'=>$rt->id(),
-                    'id_entite'=>$arr_entites[$rt->id()]['id_entite'],
-                    'cols'=>$cols
-                ];
-            } else {
-                $tables[] = [
-                    'tableName' => ucfirst($str_pro),
-                    'id' => $rt->id(),
-                    'x' => isset($arr_entites[$rt->id()]['x']) ? $arr_entites[$rt->id()]['x'] : 0,
-                    'y' => isset($arr_entites[$rt->id()]['y']) ? $arr_entites[$rt->id()]['y'] : 0,
-                    'id_entite' => $arr_entites[$rt->id()]['id_entite'],
-                    'cols' => $cols
-                ];
-            }
-        }
 
-        $keyTemp = [];
-        foreach ($tables as $key=>$t) {
-            $keyTemp[$t['id']]['key'] = $key;
-            $keyTemp[$t['id']]['name'] = $t['tableName'];
-        }
-        //consruction de la table des liens
-        foreach ($tables as $key=>$t) {
-            foreach ($t['cols'] as $i => $c) {
-                foreach ($c['links'] as $l) {
-                    $links[]=[
-                        "source"=>isset($keyTemp[$t['id']]['key']) ? $keyTemp[$t['id']]['key'] : 0,
-                        "target"=>isset($keyTemp[$l['id']]['key']) ? $keyTemp[$l['id']]['key'] : 0,
-                        "relation"=>'line1',
-                        "sourceIndex"=>$i+1,
-                        "targetIndex"=>1,
-                        "value"=>1,
-                        "nb"=>$l['nb'],
+        $arr_entites = array();
+        $arr_id_rs = array();
+        if (count($result_carte) > 0) {
+            foreach ($result_carte[0]['nodes'] as $entites) {
+                $arr_entites[$entites['idResource']]['x'] = $entites['x'];
+                $arr_entites[$entites['idResource']]['y'] = $entites['y'];
+                $arr_entites[$entites['idResource']]['id_entite'] = $entites['id'];
+                $arr_id_rs[] = $entites['idResource'];
+            }
+
+            $query_rs = [
+                'id' => $arr_id_rs,
+            ];
+            $resource_templates = $this->api->search('resource_templates', $query_rs, ['limit' => 0])->getContent();
+
+            foreach ($resource_templates as $key => $rt) {
+                //récupère les propriétés
+                $pros = $rt->resourceTemplateProperties();
+                $cols = array();
+                foreach ($pros as $pro) {
+                    $p = $pro->property();
+                    $str_title = strlen($p->label()) > 17 ? substr($p->label(), 0, 17) . "..." : $p->label();
+                    $cols[] = [
+                        'itemName' => ucfirst($str_title)
+                        , 'id' => $p->id()
+                        , 'links' => $this->getPropertyLinks($p)
                     ];
+                }
+                $str_pro = strlen($rt->label()) > 17 ? substr($rt->label(), 0, 17) . "..." : $rt->label();
+                if (!isset($arr_entites[$rt->id()]['x']) || !isset($arr_entites[$rt->id()]['y'])) {
+                    $tables[] = [
+                        'tableName' => ucfirst($str_pro),
+                        'id' => $rt->id(),
+                        'id_entite' => $arr_entites[$rt->id()]['id_entite'],
+                        'cols' => $cols
+                    ];
+                } else {
+                    $tables[] = [
+                        'tableName' => ucfirst($str_pro),
+                        'id' => $rt->id(),
+                        'x' => isset($arr_entites[$rt->id()]['x']) ? $arr_entites[$rt->id()]['x'] : 0,
+                        'y' => isset($arr_entites[$rt->id()]['y']) ? $arr_entites[$rt->id()]['y'] : 0,
+                        'id_entite' => $arr_entites[$rt->id()]['id_entite'],
+                        'cols' => $cols
+                    ];
+                }
+            }
+
+            $keyTemp = [];
+            foreach ($tables as $key => $t) {
+                $keyTemp[$t['id']]['key'] = $key;
+                $keyTemp[$t['id']]['name'] = $t['tableName'];
+            }
+            //consruction de la table des liens
+            foreach ($tables as $key => $t) {
+                foreach ($t['cols'] as $i => $c) {
+                    foreach ($c['links'] as $l) {
+                        $links[] = [
+                            "source" => isset($keyTemp[$t['id']]['key']) ? $keyTemp[$t['id']]['key'] : 0,
+                            "target" => isset($keyTemp[$l['id']]['key']) ? $keyTemp[$l['id']]['key'] : 0,
+                            "relation" => 'line1',
+                            "sourceIndex" => $i + 1,
+                            "targetIndex" => 1,
+                            "value" => 1,
+                            "nb" => $l['nb'],
+                        ];
+                    }
                 }
             }
         }
